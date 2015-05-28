3601aeb70d289e4fc42466115f6f572a03500edf
Sundar M. Vaidya <sundar@cloudfactory.com>
2015-05-28 17:43:26 +0545

    Makes axis title readable.

--- server.R
+++ server.R
@@ -10,8 +10,9 @@ function(input, output) {
                 mapping = aes(x = time_posixct, y = mag, size = mag), 
                 height = 960, width = 540) + 
       geom_point() + 
-      scale_x_datetime(breaks = date_breaks("3 hour"),
+      scale_x_datetime("Time of Day", breaks = date_breaks("3 hour"),
                        labels = date_format("%H:%M")) + 
+      scale_y_continuous("Magnitude") + 
       facet_wrap(~date, ncol = 7) + 
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
     
