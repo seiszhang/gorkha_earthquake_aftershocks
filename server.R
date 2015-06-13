76390d05f2ff5a0139cb9d27997af048ef94e22c
Sundar M. Vaidya <vaidyasm@gmail.com>
2015-06-13 14:17:26 +0545

    Server outputs dates and quake plot based on input days.

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 require('shiny', quietly = TRUE, warn.conflicts = FALSE)
+require('lubridate', quietly = TRUE, warn.conflicts = FALSE)
 
 function(input, output) {
   
@@ -32,4 +33,36 @@ function(input, output) {
   
   output$leaflet.plot.gorkha.quakes.quakes.on.map <- 
     renderLeaflet(leaflet.plot.gorkha.quakes.quakes.on.map())
+  
+  animate.map.day.slider.data <- reactive({
+    ifelse(is.integer(input$animate.map.day.slider), 
+           input$animate.map.day.slider, 
+           0)
+  })
+
+  output$animate.map.date.to <- renderText({
+    (lubridate::ymd("2015-04-25", tz = "Asia/Kathmandu") + 
+       lubridate::days(animate.map.day.slider.data())) %>% 
+      as.character()
+  })
+  
+  output$ui.animate.map.day.slider <- renderUI({
+    sliderInput(inputId = "animate.map.day.slider", 
+                label = "Days: ", 
+                min = 0, max = difftime(time1 = Sys.Date() %>% 
+                                  as.POSIXct(), 
+                                  time2 = lubridate::ymd("2015-04-25"), 
+                                  units = "days") %>% 
+                  ceiling() %>% 
+                  as.integer(), 
+                value = 0, step = 1, 
+                animate = animationOptions(interval = 500, loop = TRUE))
+  })
+  
+  output$animate.map.plot <- renderPlot({
+    (lubridate::ymd("2015-04-25", tz = "Asia/Kathmandu") + 
+       lubridate::days(animate.map.day.slider.data()) + 
+       lubridate::hms("23:59:59")) %>% 
+      plot.gorkha.quakes.to.date()
+  })
 }
